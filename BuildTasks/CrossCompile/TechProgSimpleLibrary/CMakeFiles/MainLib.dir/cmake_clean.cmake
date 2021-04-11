f0b3ae1cc079e2280ad876bda31112ba3af6b335
staketd <pushind00@yandex.ru>
2021-04-11 17:32:38 +0300

    cross compile task

---
+++ BuildTasks/CrossCompile/TechProgSimpleLibrary/CMakeFiles/MainLib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MainLib.dir/main_lib.cpp.o"
+  "CMakeFiles/MainLib.dir/main_lib.cpp.o.d"
+  "bin/MainLib"
+  "bin/MainLib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MainLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
